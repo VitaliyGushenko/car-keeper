76335f2f01220fc038e95b43d162c027b75deda8
DESKTOP-GEDM3JE\Vitaliy <gushenko759@gmai.com>
2026-09-19 09:17:17 +0300

    feat: firebase config, providers (auth, firestore, storage, analytics) and build settings

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,22 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { getAnalytics, provideAnalytics } from '@angular/fire/analytics';
 
 import { routes } from './app.routes';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics()),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage()),
+  ],
 };
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBmYpMopML4tEbvwceDXKFvgHG54iNkkf0',
+    authDomain: 'car-keeper-a97bb.firebaseapp.com',
+    projectId: 'car-keeper-a97bb',
+    storageBucket: 'car-keeper-a97bb.firebasestorage.app',
+    messagingSenderId: '913525844665',
+    appId: '1:913525844665:web:317d36452ffdfa8e588968',
+    measurementId: 'G-2ZRHLF994R',
+  },
+};
